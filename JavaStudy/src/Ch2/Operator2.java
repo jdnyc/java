94ec56dd5dd2c288684dcc2c8491cf574a77eb86
장윤성 <dbstjd1014@naver.com>
2022-12-07 18:12:11 +0900

    2-2 단항연산자/부호연산자 (부호변경)

---
+++ JavaStudy/src/Ch2/Operator2.java
@@ -0,0 +1,16 @@
+package Ch2;
+
+public class Operator2 {
+	//부호변경
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i;
+		//부호를 -에서 +로 변경
+		System.out.println(i);
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
+
+}
